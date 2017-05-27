a868e2afcf3c1039c0525c544ab803e27861598f
Greensue <1719837353@qq.com>
2017-05-27 10:01:59 +0800

    add GOTHook example

---
+++ AndroidInjection-master/exercise3-GOTHook/libhook/obj/local/arm64-v8a/objs/hook/libhook.o.d
@@ -0,0 +1,5 @@
+/Users/greensue/Downloads/AndroidInjection-master/exercise3-GOTHook/libhook/obj/local/arm64-v8a/objs/hook/libhook.o: \
+  /Users/greensue/Downloads/AndroidInjection-master/exercise3-GOTHook/libhook/jni/libhook.c \
+  /Users/greensue/Downloads/AndroidInjection-master/exercise3-GOTHook/libhook/jni/linker.h
+
+/Users/greensue/Downloads/AndroidInjection-master/exercise3-GOTHook/libhook/jni/linker.h:
